4a1112778a07bcbb7f597c4cdccadf81e9b6cc04
♡ gabby ♡ <81900622+GabrielBuckingham@users.noreply.github.com>
2023-07-14 21:20:24 -0700

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class Game {
+public:
+	sf::RenderWindow* window = nullptr;
+	sf::Event* event = nullptr;
+	int width, height;
+
+	Game(int width, int height) {
+		this->width = width;
+		this->height = height;
+		window = new sf::RenderWindow(sf::VideoMode(width, height), "CUDA Galaxy Simulation", sf::Style::Close);
+		event = new sf::Event();
+	}
+
+	~Game() {
+		delete window;
+		delete event;
+	}
+
+	int numPixels() {
+		return width * height;
+	}
+
+	bool open() {
+		return window->isOpen();
+	}
+
+	void eventHandler() {
+		while (window->pollEvent(*event)) {
+			if (event->type == sf::Event::Closed) {
+				window->close();
+				std::cout << "Simulation Ended\n";
+			}
+			if (event->key.code == sf::Keyboard::Enter) {
+				window->close();
+				std::cout << "Simulation Ended\n";
+			}
+		}
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "kernel.cuh"
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "Game.h"
+
+
+int main() {
+	Game game(500, 500);
+
+	int arraySize = 5;
+	int* a = new int[arraySize] {1, 2, 3, 4, 5};
+	int* b = new int[arraySize] {10, 20, 30, 40, 50};
+	int* c = new int[arraySize];
+
+	go(a, b, c, arraySize);
+
+	std::cout << "{1, 2, 3, 4, 5} + {10, 20, 30, 40, 50} = {" << c[0] << ", " << c[1] << ", " << c[2] << ", " << c[3] << ", " << c[4] << "}\n";
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+
+	while (game.open()) {
+		game.eventHandler();
+		game.window->clear(sf::Color(0, 0, 0));
+		game.window->display();
+	}
+
+	return 0;
+}
